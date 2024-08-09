ce9085b8101669b5ccb4e79356e568bfff8b5494
Navya07122001 <navyasreeparvatham@gmail.com>
2024-08-09 14:15:00 +0530

    counterapp implemented

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import XClassComp from './Components/XClassComp';
+
+function App() {
+  return (
+    <div className="App">
+      <XClassComp/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/XClassComp.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class XClassComp extends Component {
+   constructor(props) {
+        super(props);
+        this.state={
+            counter:0,
+       };
+        this.handleDecrement=this.handleDecrement.bind(this);
+        this.handleIncrement=this.handleIncrement.bind(this);
+
+    }
+    handleDecrement(){
+        this.setState({
+            counter:this.state.counter-1
+        })
+       }
+    handleIncrement(){
+       this.setState({
+        counter:this.state.counter+1
+       })
+       }
+ 
+    // componentWillMount() {
+
+    // }
+
+    // componentDidMount() {
+
+
+    // }
+
+    render() {
+        return (
+            <div>
+            <h1>Counter App</h1>
+            <p>{this.state.counter}</p>
+            <button type="button" onClick={this.handleIncrement} style={{width:'auto',height:'auto'}}>Increment</button>
+            <button type="button" onClick={this.handleDecrement} style={{width:'auto',height:'auto'}}>Decrement</button>
+
+            </div>
+        );
+    }
+}
+
+
+export default XClassComp;
